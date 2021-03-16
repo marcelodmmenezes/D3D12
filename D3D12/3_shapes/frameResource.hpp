73852829d55d441715ad713a1c3bbccf31e6edbb
Marcelo Menezes <marcelodmmenezes@gmail.com>
2021-03-16 18:47:03 -0300

    Shapes

--- D3D12/3_shapes/frameResource.hpp
+++ D3D12/3_shapes/frameResource.hpp
@@ -34,10 +34,23 @@ struct Vertex
 
 struct FrameResource
 {
-	FrameResource(ID3D12Device* device, UINT pass_count, UINT object_count);
+	FrameResource(ID3D12Device* device, UINT pass_count, UINT object_count)
+	{
+		THROW_IF_FAILED(device->CreateCommandAllocator(
+			D3D12_COMMAND_LIST_TYPE_DIRECT,
+			IID_PPV_ARGS(&command_list_allocator)));
+
+		pass_cb = std::make_unique<UploadBuffer<PassConstants>>(device, pass_count, true);
+		object_cb = std::make_unique<UploadBuffer<ObjectConstants>>(device, object_count, true);
+	}
+
+	~FrameResource()
+	{
+
+	}
+
 	FrameResource(FrameResource const&) = delete;
 	FrameResource& operator=(FrameResource const&) = delete;
-	~FrameResource();
 
 	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> command_list_allocator;
 
